a35fc42478ddea452a74c293154368dae4affd6e
cjh-cloud <clooneyjh@gmail.com>
2023-01-04 21:47:56 +0000

    [terraform] add tf file in modules dir

---
+++ modules/main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "a" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "policy" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
