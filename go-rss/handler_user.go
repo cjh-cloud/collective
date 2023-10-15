2c39313724b609f7abcba583e80bc3715d980539
cjh-cloud <clooneyjh@gmail.com>
2023-10-15 16:13:09 +1300

    [go-rss] authentication w api keys

--- go-rss/handler_user.go
+++ go-rss/handler_user.go
@@ -35,5 +35,10 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	}
 	
 	// Empty struct, marshal to payload object
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+// Authenticated endpoint
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
